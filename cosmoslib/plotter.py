0ccc16807051b345b380b52515ab2b6fd527b5e8
Yilun Guan <zoom.aaron@gmail.com>
2019-05-14 14:19:43 -0400

    Make plotter compabible with scalar output from camb

--- cosmoslib/plotter.py
+++ cosmoslib/plotter.py
@@ -6,10 +6,15 @@ def plot_ps(total_ps, axes=None, **kwargs):
     if not np.any(axes):
         fig, axes = plt.subplots(2,2,figsize=(12,9))
 
-    axes[0,0].loglog(total_ps[:,0],total_ps[:,1], **kwargs)
-    axes[0,1].loglog(total_ps[:,0],total_ps[:,2], **kwargs)
-    axes[1,0].loglog(total_ps[:,0],total_ps[:,3], **kwargs)
-    axes[1,1].loglog(total_ps[:,0],np.abs(total_ps[:,4]), **kwargs)
+    if total_ps.shape[-1] == 5:
+        axes[0,0].loglog(total_ps[:,0],total_ps[:,1], **kwargs)
+        axes[0,1].loglog(total_ps[:,0],total_ps[:,2], **kwargs)
+        axes[1,0].loglog(total_ps[:,0],total_ps[:,3], **kwargs)
+        axes[1,1].loglog(total_ps[:,0],np.abs(total_ps[:,4]), **kwargs)
+    elif total_ps.shape[-1] == 4:
+        axes[0,0].loglog(total_ps[:,0],total_ps[:,1], **kwargs)
+        axes[0,1].loglog(total_ps[:,0],total_ps[:,2], **kwargs)
+        axes[1,1].loglog(total_ps[:,0],np.abs(total_ps[:,3]), **kwargs)
 
     for i, label in enumerate(['TT','EE','BB','TE']):
         ax = axes[i//2,i%2]
