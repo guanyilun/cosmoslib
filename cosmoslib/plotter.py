70da43a55ed52ff08975571c272681bb18626a63
Yilun Guan <zoom.aaron@gmail.com>
2019-05-16 22:54:12 -0400

    Add support to plot format

--- cosmoslib/plotter.py
+++ cosmoslib/plotter.py
@@ -1,20 +1,20 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
-def plot_ps(total_ps, axes=None, **kwargs):
+def plot_ps(total_ps, fmt="-", axes=None, **kwargs):
     """Plot the power spectra"""
     if not np.any(axes):
         fig, axes = plt.subplots(2,2,figsize=(12,9))
 
     if total_ps.shape[-1] == 5:
-        axes[0,0].loglog(total_ps[:,0],total_ps[:,1], **kwargs)
-        axes[0,1].loglog(total_ps[:,0],total_ps[:,2], **kwargs)
-        axes[1,0].loglog(total_ps[:,0],total_ps[:,3], **kwargs)
-        axes[1,1].loglog(total_ps[:,0],np.abs(total_ps[:,4]), **kwargs)
+        axes[0,0].loglog(total_ps[:,0],total_ps[:,1], fmt, **kwargs)
+        axes[0,1].loglog(total_ps[:,0],total_ps[:,2], fmt, **kwargs)
+        axes[1,0].loglog(total_ps[:,0],total_ps[:,3], fmt, **kwargs)
+        axes[1,1].loglog(total_ps[:,0],np.abs(total_ps[:,4]), fmt, **kwargs)
     elif total_ps.shape[-1] == 4:
-        axes[0,0].loglog(total_ps[:,0],total_ps[:,1], **kwargs)
-        axes[0,1].loglog(total_ps[:,0],total_ps[:,2], **kwargs)
-        axes[1,1].loglog(total_ps[:,0],np.abs(total_ps[:,3]), **kwargs)
+        axes[0,0].loglog(total_ps[:,0],total_ps[:,1], fmt, **kwargs)
+        axes[0,1].loglog(total_ps[:,0],total_ps[:,2], fmt, **kwargs)
+        axes[1,1].loglog(total_ps[:,0],np.abs(total_ps[:,3]), fmt, **kwargs)
 
     for i, label in enumerate(['TT','EE','BB','TE']):
         ax = axes[i//2,i%2]
