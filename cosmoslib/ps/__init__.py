8946d9ce1f77550fd37a3ab5c5f5ef21563a591d
Yilun Guan <zoom.aaron@gmail.com>
2020-04-13 13:21:30 -0400

    ps: not build cython package by default

--- cosmoslib/ps/__init__.py
+++ cosmoslib/ps/__init__.py
@@ -1,3 +1,1 @@
-from .ps import PS, SimpleNoise, add_prefactor, remove_prefactor, \
-    resample, N_l, add_noise_nl, Dl2Cl, Cl2Dl, gen_ps_realization, \
-    covmat
+from .ps import *
--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@
 """The setup script."""
 
 from setuptools import setup, find_packages
-from distutils.extension import Extension
-from Cython.Build import cythonize
-import numpy as np
+# from distutils.extension import Extension
+# from Cython.Build import cythonize
+# import numpy as np
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
@@ -43,13 +43,14 @@ setup(
     url='https://github.com/guanyilun/cosmoslib',
     version='0.1.0',
     zip_safe=False,
-    ext_modules=cythonize([
-        Extension(
-            name="cosmoslib.ps._ps",
-            sources=["cosmoslib/ps/ps.pyx"],
-            include_dirs=[np.get_include()],
-            extra_compile_args=['-fopenmp'],
-            extra_link_args=['-fopenmp'],
-        )
-    ])
+    # not used build by default
+    # ext_modules=cythonize([
+    #     Extension(
+    #         name="cosmoslib.ps._ps",
+    #         sources=["cosmoslib/ps/ps.pyx"],
+    #         include_dirs=[np.get_include()],
+    #         extra_compile_args=['-fopenmp'],
+    #         extra_link_args=['-fopenmp'],
+    #     )
+    # ])
 )
