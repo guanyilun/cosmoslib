95605893a993e5981e32dfc856bb40230612e24c
Yilun Guan <zoom.aaron@gmail.com>
2020-04-20 01:21:54 -0400

    add some quadratic estimators

--- setup.py
+++ setup.py
@@ -3,10 +3,13 @@
 
 """The setup script."""
 
-from setuptools import setup, find_packages
-from distutils.extension import Extension
-from Cython.Build import cythonize
+from setuptools import find_packages
+# for building fortran packages
+from numpy.distutils.core import setup, Extension
 import numpy as np
+# for building cython packages
+from Cython.Build import cythonize
+
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
@@ -16,6 +19,23 @@ with open('HISTORY.rst') as history_file:
 
 requirements = ['numpy', 'scipy', 'matplotlib',
                 'cython', 'cytoolz', 'healpy', 'configparser']
+
+cython_modules = cythonize(Extension(
+    name="cosmoslib.like._likelihood",
+    sources=["cosmoslib/like/_likelihood.pyx"],
+    include_dirs=[np.get_include()],
+    # extra_compile_args=['-fopenmp'],
+    # extra_link_args=['-fopenmp'],
+))
+
+f2py_modules = [
+    Extension(
+        name='cosmoslib.utils.cwignerd',
+        sources=['deps/src/wignerd/wignerd.pyf','deps/src/wignerd/wignerd.c']
+    )
+]
+
+ext_modules = cython_modules + f2py_modules
 
 setup(
     author="Yilun Guan",
@@ -39,13 +59,5 @@ setup(
     url='https://github.com/guanyilun/cosmoslib',
     version='0.1.0',
     zip_safe=False,
-    ext_modules=cythonize([
-        Extension(
-            name="cosmoslib.like._likelihood",
-            sources=["cosmoslib/like/_likelihood.pyx"],
-            include_dirs=[np.get_include()],
-            # extra_compile_args=['-fopenmp'],
-            # extra_link_args=['-fopenmp'],
-        )
-    ])
+    ext_modules=ext_modules
 )
